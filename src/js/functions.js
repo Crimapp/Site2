a8c3ba6b08bc4bde2bba0a70b80d246218c38562
Lucas-Uchiha <lucas@uchiha.xyz>
2017-07-11 21:41:21 -0300

    Versão 0.5

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,10 +1,33 @@
-window.addEventListener("resize",mapResize);
-window.onload = mapResize;
+window.addEventListener("resize",windowResize);
+var dialog;
+window.onload = function(){
+    windowResize();
+    getLocation();
+    google.maps.event.trigger(map, 'resize')
+    // Add suport to dialog element on browsers that doens't support it
+    dialog = document.querySelector('dialog');
+    if (! dialog.showModal) {
+        dialogPolyfill.registerDialog(dialog);
+    }
+}
 
-function mapResize(){
-	var sizeH = document.getElementById("header").offsetHeight;
-    var sizeF = document.getElementById("footer").offsetHeight;
-    var h = window.outerHeight - sizeF - sizeH - 125;
-    
-    document.getElementById("map").style.height = h + "px";
+function windowResize(){
+    if(!document.querySelector(".is-small-screen")){
+        document.querySelector(".mdl-layout-title").style = "margin-left: -55px;";
+        document.getElementById("demo-menu-lower-right").style = "margin-right: -10px;";
+    }
+    else{
+        document.querySelector(".mdl-layout-title").style = "margin-left: 0px;";
+        document.getElementById("demo-menu-lower-right").style = "margin-right: 0px;";
+    }
+}
+
+// HTML5 geolocation
+function getLocation(){
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(changeLocation);
+    }
+    else{
+        alert("O seu navegador não suporta Geolocalização."); // muda isso depois
+    }
 }
